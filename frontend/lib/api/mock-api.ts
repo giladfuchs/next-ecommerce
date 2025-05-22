bcc38a5985339bf8291c169529a0e474d2e164d4
giladfuchs <igilfu@gmail.com>
2025-05-22 15:33:46 +0300

    add mock data for vercel requirements11

--- frontend/lib/api/mock-api.ts
+++ frontend/lib/api/mock-api.ts
@@ -1,6 +1,6 @@
 import {array_obj_to_obj_with_key} from "../types";
-import {mockData} from "./mock-data";
 
+import mockData from "./mock-data.json";
 
 export async function mockResponse(input: string): Promise<Response> {
 
@@ -8,13 +8,7 @@ export async function mockResponse(input: string): Promise<Response> {
     let json;
 
     if (input === "/data") {
-        // json = {
-        //
-        //     products: [],
-        //     categories:[]
-        // };
         json = {
-
             products: mockData.products,
             categories: mockData.categories,
         };
