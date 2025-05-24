0f85f836caf251d4cff345e000f7a912dd21a061
giladfuchs <igilfu@gmail.com>
2025-05-25 00:18:35 +0300

    fix images

--- components/products/single/product-gallery.tsx
+++ components/products/single/product-gallery.tsx
@@ -5,26 +5,18 @@ import Image from "next/image";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { GridTileImage } from "components/products/grid/tile";
-import { localeCache } from "lib/api";
 import { ProductImage } from "lib/types";
+import { localeCache } from "lib/api";
 
-export function ProductGallery({ images }: { images: ProductImage[] }) {
+export const ProductGallery = ({ images }: { images: ProductImage[] }) => {
   const [imageIndex, setImageIndex] = useState(0);
-  const isRtl = localeCache.isRtl();
   const next = () => setImageIndex((prev) => (prev + 1) % images.length);
-
   const prev = () =>
     setImageIndex((prev) => (prev - 1 + images.length) % images.length);
 
-  const handleNext = () => (isRtl ? prev() : next());
-  const handlePrev = () => (isRtl ? next() : prev());
-
   return (
     <div>
-      <div
-        className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden"
-        dir={localeCache.dir()}
-      >
+      <div className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden">
         <Image
           className="h-full w-full object-contain mx-auto"
           fill
@@ -36,35 +28,26 @@ export function ProductGallery({ images }: { images: ProductImage[] }) {
 
         {images.length > 1 && (
           <div className="absolute bottom-[15%] flex w-full justify-center">
-            <div className="mx-auto flex h-11 items-center rounded-full border border-white bg-neutral-50/80 text-neutral-500 backdrop-blur-sm dark:border-black dark:bg-neutral-900/80">
+            <div
+              dir="ltr"
+              className="mx-auto flex h-11 items-center rounded-full border border-white bg-neutral-50/80 text-neutral-500 backdrop-blur-sm dark:border-black dark:bg-neutral-900/80"
+            >
               <button
-                onClick={handleNext}
-                aria-label={
-                  isRtl ? "Previous product image" : "Next product image"
-                }
+                onClick={localeCache.isRtl() ? next : prev}
+                aria-label="Previous product image"
                 className="h-full px-6 transition-all hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center"
               >
-                {isRtl ? (
-                  <ChevronRightIcon fontSize="small" />
-                ) : (
-                  <ChevronLeftIcon fontSize="small" />
-                )}
+                <ChevronLeftIcon fontSize="small" />
               </button>
 
               <div className="mx-1 h-6 w-px bg-neutral-500" />
 
               <button
-                onClick={handlePrev}
-                aria-label={
-                  isRtl ? "Next product image" : "Previous product image"
-                }
+                onClick={localeCache.isRtl() ? prev : next}
+                aria-label="Next product image"
                 className="h-full px-6 transition-all hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center"
               >
-                {isRtl ? (
-                  <ChevronLeftIcon fontSize="small" />
-                ) : (
-                  <ChevronRightIcon fontSize="small" />
-                )}
+                <ChevronRightIcon fontSize="small" />
               </button>
             </div>
           </div>
@@ -94,4 +77,4 @@ export function ProductGallery({ images }: { images: ProductImage[] }) {
       )}
     </div>
   );
-}
+};
