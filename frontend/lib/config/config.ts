ecd67914cf853e98ac227aba58fe6fb1a95f39e7
giladfuchs <igilfu@gmail.com>
2025-07-09 18:08:23 +0300

    begin move to next 15

--- frontend/lib/config/config.ts
+++ frontend/lib/config/config.ts
@@ -5,12 +5,12 @@ const port = process.env.NEXT_PUBLIC_PORT || "4000";
 export const baseUrl: string =
   process.env.NEXT_PUBLIC_BASE_URL?.trim() || "https://your-store.vercel.app";
 
-export const API_URL: string =
-  isDev || isTest
-    ? `http://localhost:${port}`
-    : process.env.NEXT_PUBLIC_API_URL?.trim() || `${baseUrl}/api`;
+// export const API_URL: string =
+//   isDev || isTest
+//     ? `http://localhost:${port}`
+//     : process.env.NEXT_PUBLIC_API_URL?.trim() || `${baseUrl}/api`;
 
-// export const API_URL: string = process.env.NEXT_PUBLIC_API_URL
+export const API_URL: string = process.env.NEXT_PUBLIC_API_URL
 
 export const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME || "ECOMMERCE";
 export const USE_MOCK_DATA = process.env.NEXT_PUBLIC_USE_MOCK_DATA === "true";
@@ -24,7 +24,7 @@ export const FOOTER_DATA =
 export const WHATSAPP_MESSAGE = process.env.NEXT_PUBLIC_WHATSAPP_MESSAGE;
 export const ICON_IMAGE_URL =
   process.env.NEXT_PUBLIC_ICON_IMAGE_URL ??
-  "https://racit0uja2cckwpw.public.blob.vercel-storage.com/logo-social.png";
+  "https://racit0uja2cckwpw.public.blob.vercel-storage.com/logo.png";
 
 // Max upload size: 1 MB (easy to tweak to 2 or 1.5)
 export const MAX_FILE_SIZE_MB = 1 * 1024 * 1024;
