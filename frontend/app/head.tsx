cd4482b05c67d078fc01d8bfc1b408ae11d71ab5
giladfuchs <igilfu@gmail.com>
2025-07-18 11:31:14 +0300

    small clean!

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -1,8 +1,15 @@
-export default function Head() {
-  return (
-    <>
-      <meta name="robots" content="index,follow" />
-    </>
-  );
-}
+import { API_URL } from "@/lib/config";
 
+export default function Head() {
+    return (
+        <>
+            <meta name="robots" content="index,follow" />
+
+            <link rel="preconnect" href={API_URL} crossOrigin="anonymous" />
+            <link rel="dns-prefetch" href={API_URL} />
+
+            {/* Fallback icon */}
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+        </>
+    );
+}
