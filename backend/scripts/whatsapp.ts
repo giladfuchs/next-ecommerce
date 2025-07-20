9c2cc31228f104d80219b740e4dc022ff12abae6
giladfuchs <igilfu@gmail.com>
2025-07-20 16:46:26 +0300

    clean script

--- backend/scripts/whatsapp.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import {sendAdminWhatsApp} from "../src/lib/service";
-
-dotenv.config();
-sendAdminWhatsApp(3).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
